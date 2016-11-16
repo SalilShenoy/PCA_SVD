e9f018b49598e1e8229786f5548d43e38ec3578c
Salil Shenoy <salilshenoy@gmail.com>
2016-11-15 17:00:51 -0800

    Implemented SVM for classification

--- pca_sample.py
+++ pca_sample.py
@@ -4,7 +4,7 @@
 #imports
 import csv
 import numpy as np
-from sklearn import preprocessing
+from sklearn import preprocessing, cross_validation, svm
 from sklearn.decomposition import PCA
 
 ### CONSTANTS:
@@ -75,12 +75,26 @@ nan_locs = []
 row_index = 0
 
 data = read_data(cr, data, data_header)
-test_data = read_data(test, data_test, data_test_header)
+data_test = read_data(test, data_test, data_test_header)
+
+class_info_train = np.array(map(lambda x: int(float(x)), data[:,2]))
+# Make sure class is either 0 or 1. If so, append it to class_info list else raise error.
+for c in class_info_train:
+    if c not in [0,1]:
+        raise ValueError("The column named ",header[2], " in example_svm_train.csv has a value not equal to 0 or 1.")        
+y_train = np.array(class_info_train, dtype = int)
+
+class_info_test = np.array(map(lambda x: int(float(x)), data_test[:,2]))
+# Make sure class is either 0 or 1. If so, append it to class_info list else raise error.
+for c in class_info_test:
+    if c not in [0,1]:
+        raise ValueError("The column named ",header_test[2], " in example_svm_train.csv has a value not equal to 0 or 1.")       
+y_test = np.array(class_info_test, dtype = int)
 
 X = np.array(data[:,3:], dtype = float)
 X = preprocessing.scale(X)
 
-X_test = np.array(test_data[:,3:], dtype = float)
+X_test = np.array(data_test[:,3:], dtype = float)
 X_test = preprocessing.scale(X_test)
 
 percent_covariance_to_account_for = 0.7
@@ -88,19 +102,6 @@ component = 0
 tempSum = 0.0
 
 pca = doPCA(X)
-'''
-print
-print
-print pca.explained_variance_ratio_
-print
-print
-print pca.explained_variance_ratio_.shape
-print
-print
-print pca.explained_variance_ratio_.sum()
-print
-print
-'''
 
 while (True):
     if (tempSum < percent_covariance_to_account_for):
@@ -137,21 +138,30 @@ print
 print X
 print
 print X_test
-"""
-print pca.explained_variance_
+
+#SVM
+num_class_0_train = list(y_train).count(0)
+num_class_1_train = list(y_train).count(1)
+cv_size_train = min(num_class_0_train, num_class_1_train)
+
+num_class_0_test = list(y_test).count(0)
+num_class_1_test = list(y_test).count(1)
+cv_size_test = min(num_class_0_test, num_class_1_test)
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y_train,random_state=0)
+print "X_train shape =", X_train.shape, "  y_train shape=", y_train.shape
+print "X_test shape =", X_test.shape, "  y_test shape=", y_test.shape
 print
-print
-print pca.get_params()
-print 
-print
-print 'transforming PCA'
-print 
-print 
-pca_trans =  pca.fit_transform(data, (X.shape[0], X.shape[1])) #data , shape (n_samples, n_features) so I have 243 samples and 3 features ??
-print 
-print
-print pca_trans
-print
-print
-print pca.get_params()
-"""
+clf = svm.SVC(kernel='rbf', C=1, gamma = 0.0, degree = 3.0, coef0 = 0.0).fit(X_train, y_train)
+
+#print "clf.get_params(deep=True) =", clf.get_params(deep=True)
+
+print "clf.score(X_test, y_test) = {0}%".format(int((clf.score(X_test, y_test) * 10000))/100.)
+#print "clf.predict(X_test) = ", clf.predict(X_test)
+#print "clf.decision_function(X_test) = ", clf.decision_function(X_test)
+
+#print "======================="
+
+#print "clf.score(X_train, y_train) = {0}%".format(int((clf.score(X_train, y_train) * 10000))/100.)
+#print "clf.predict(X_train) = ", clf.predict(X_train)
+#print "clf.decision_function(X_train) = ", clf.decision_function(X_train)
